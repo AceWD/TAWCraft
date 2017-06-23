bb0aef4d4a7488b8d17b60d3ac6b15e56a5e0fb5
AceWD <shalev.goo@gmail.com>
2017-06-24 00:28:34 +0300

    Initial commit

---
+++ app/src/main/java/xywd/tawcraft/MainActivity.java
@@ -0,0 +1,14 @@
+package xywd.tawcraft;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //Hello bkaa
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
