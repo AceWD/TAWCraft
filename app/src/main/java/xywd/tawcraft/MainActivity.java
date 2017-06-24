2e6f66b2817311c6cdb9004d6cb87f75c2986f12
Xylem-Gaming <xylemgaming.game@gmail.com>
2017-06-24 20:44:38 +0100

    Xylem Test

--- app/src/main/java/xywd/tawcraft/MainActivity.java
+++ app/src/main/java/xywd/tawcraft/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //Hello bkaad
+        //Hello Ace
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
