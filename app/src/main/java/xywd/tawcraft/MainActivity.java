adeec846162ae144bcfa6acee632b3ea3c9b851d
AceWD <shalev.goo@gmail.com>
2017-07-01 00:24:10 +0300

    Servers Activity is almost done, just need to add the rest of the servers but the code is there

--- app/src/main/java/xywd/tawcraft/MainActivity.java
+++ app/src/main/java/xywd/tawcraft/MainActivity.java
@@ -1,14 +1,33 @@
 package xywd.tawcraft;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    Button servers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //Hello XylemWD
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        Button servers = (Button) findViewById(R.id.serversButton);
+
+        servers.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Intent i;
+
+        i = new Intent(this,ServersActivity.class);
+        startActivity(i);
     }
 }
